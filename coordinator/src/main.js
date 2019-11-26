4c16d4411890a207a535e98d9231636a7adc7aa7
Alberto Barrajón <tolletum@gmail.com>
2019-11-26 22:46:28 +0100

    feat(lint): disabled lint

--- coordinator/src/main.js
+++ coordinator/src/main.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import VueResource from 'vue-resource'
-import App from './App.vue'
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:8081/coordinators';
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount('#app');
